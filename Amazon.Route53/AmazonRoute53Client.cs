7264db5c8d00c6f248785083b06502e9441b4d7a
Michael Friis <friism@gmail.com>
2011-08-15 19:57:28 -0700

    Add basics

---
+++ Amazon.Route53/AmazonRoute53Client.cs
@@ -0,0 +1,55 @@
+/*
+ * Copyright 2010-2011 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License").
+ * You may not use this file except in compliance with the License.
+ * A copy of the License is located at
+ * 
+ *  http://aws.amazon.com/apache2.0
+ * 
+ * or in the "license" file accompanying this file. This file is distributed
+ * on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
+ * express or implied. See the License for the specific language governing
+ * permissions and limitations under the License.
+ */
+using System;
+
+using Amazon.CloudFormation.Model;
+using Amazon.CloudFormation.Model.Transform;
+using Amazon.Runtime;
+using Amazon.Runtime.Internal;
+using Amazon.Runtime.Internal.Auth;
+using Amazon.Runtime.Internal.Transform;
+
+namespace Amazon.Route53
+{
+    public class AmazonRoute53Client : AmazonWebServiceClient, AmazonRoute53
+    {
+        AbstractAWSSigner signer = new QueryStringSigner();
+
+        /// <summary>
+        /// Constructs AmazonRoute53Client with AWS Access Key ID and AWS Secret Key
+        /// </summary>
+        /// <param name="awsAccessKeyId">AWS Access Key ID</param>
+        /// <param name="awsSecretAccessKey">AWS Secret Access Key</param>
+        public AmazonRoute53Client(string awsAccessKeyId, string awsSecretAccessKey)
+            : this(awsAccessKeyId, awsSecretAccessKey, new AmazonRoute53Config())
+        {
+        }
+
+        /// <summary>
+        /// Constructs AmazonRoute53Client with AWS Access Key ID, AWS Secret Key and an
+        /// AmazonRoute53Client Configuration object. If the config object's
+        /// UseSecureStringForAwsSecretKey is false, the AWS Secret Key
+        /// is stored as a clear-text string. Please use this option only
+        /// if the application environment doesn't allow the use of SecureStrings.
+        /// </summary>
+        /// <param name="awsAccessKeyId">AWS Access Key ID</param>
+        /// <param name="awsSecretAccessKey">AWS Secret Access Key</param>
+        /// <param name="clientConfig">The AmazonCloudFormationClient Configuration Object</param>
+        public AmazonRoute53Client(string awsAccessKeyId, string awsSecretAccessKey, AmazonRoute53Config clientConfig)
+            : base(awsAccessKeyId, awsSecretAccessKey, clientConfig)
+        {
+        }
+    }
+}
